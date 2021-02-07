778103a1bc90847df73e78b137bf9e3fcca4fadf
egesha <egesha@danwin1210.me>
2021-02-07 20:59:00 +0300

    :sparkles: Add register thunk

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -12,6 +12,17 @@ const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
   }
 });
 
+const register = createAsyncThunk("auth/register", async (user, thunkAPI) => {
+  const { rejectWithValue } = thunkAPI;
+
+  try {
+    const response = await Backend.call("auth/register", user);
+    return response.token;
+  } catch (e) {
+    return rejectWithValue(e);
+  }
+});
+
 const authSlice = createSlice({
   name: "auth",
   initialState: {
@@ -26,6 +37,15 @@ const authSlice = createSlice({
       state.status = "idle";
     },
     [login.rejected]: (state, { payload }) => {
+      state.error = payload;
+      state.status = "idle";
+    },
+    [register.pending]: (state) => (state.status = "pending"),
+    [register.fulfilled]: (state, { token }) => {
+      localStorage.setItem("token", token);
+      state.status = "idle";
+    },
+    [register.rejected]: (state, { payload }) => {
       state.error = payload;
       state.status = "idle";
     },
