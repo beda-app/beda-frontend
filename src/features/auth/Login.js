0fc83ef1401242507e6a1655255f14f34ead66dc
egesha <egesha@danwin1210.me>
2021-02-07 14:33:12 +0300

    :sparkles: Rename login -> auth

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Login.scss";
+import "./SignIn.scss";
 import UserIcon from "../../assets/img/icon-user.svg";
 import PasswordIcon from "../../assets/img/icon-password.svg";
 import Input from "../../common/components/Input";
@@ -9,11 +9,11 @@ import Button from "../../common/components/Button";
 class Login extends React.Component {
   render() {
     return (
-      <div className="Login">
-        <div className="Login__container">
-          <div className="Login__title">
-            <div className="Login__title-box" />
-            <div className="Login__title-text">Вход на сайт</div>
+      <div className="SignIn">
+        <div className="SignIn__container">
+          <div className="SignIn__title">
+            <div className="SignIn__title-box" />
+            <div className="SignIn__title-text">Вход на сайт</div>
           </div>
           <Input
             left={<Icon src={UserIcon} height={48} width={38} />}
@@ -25,7 +25,7 @@ class Login extends React.Component {
             placeholder="Пароль"
             type="password"
           />
-          <div className="Login__button-container">
+          <div className="SignIn__button-container">
             <Button mode="primary">Войти</Button>
             <Button mode="secondary">Регистрация</Button>
           </div>
