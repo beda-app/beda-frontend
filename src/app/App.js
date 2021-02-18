35341c9cf666a27b7c96cb5b87f1a6fb3d90ce06
egesha <egesha@danwin1210.me>
2021-02-18 17:29:56 +0300

    :zap: Add lazy loading

--- src/app/App.js
+++ src/app/App.js
@@ -1,24 +1,28 @@
-import React from "react";
-import Login from "../features/auth/Login";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Register from "../features/auth/Register";
 import { Provider } from "react-redux";
 import store from "./store";
 import PrivateRoute from "../common/components/PrivateRoute";
-import Dashboard from "../features/dashboard/Dashboard";
+import Loading from "../common/components/Loading";
+
+const Login = lazy(() => import("../features/auth/Login"));
+const Register = lazy(() => import("../features/auth/Register"));
+const Dashboard = lazy(() => import("../features/dashboard/Dashboard"));
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
         <Router>
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <PrivateRoute path="/dashboard">
-              <Dashboard />
-            </PrivateRoute>
-          </Switch>
+          <Suspense fallback={<Loading />}>
+            <Switch>
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <PrivateRoute path="/dashboard">
+                <Dashboard />
+              </PrivateRoute>
+            </Switch>
+          </Suspense>
         </Router>
       </Provider>
     );
