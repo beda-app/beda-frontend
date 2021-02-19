60635f2cc60522dfe828020233eedcd5abe18440
egesha <egesha@danwin1210.me>
2021-02-19 20:14:22 +0300

    :art: Add env var for api url

--- src/common/backend/Backend.js
+++ src/common/backend/Backend.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:9000";
 
 export class BackendError extends Error {
   constructor(message) {
