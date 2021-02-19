80c3f8e39ef07c540cd3a5f8436cc8cc431181f7
egesha <egesha@danwin1210.me>
2021-02-19 21:02:30 +0300

    :bug: Fix dashboard.statistics keys and add offset & count on /weight/get

--- src/features/dashboard/dashboardSlice.js
+++ src/features/dashboard/dashboardSlice.js
@@ -5,13 +5,26 @@ export const fetchWeights = createAsyncThunk(
   "dashboard/fetchWeights",
   async (timeDelta, thunkAPI) => {
     const { rejectWithValue } = thunkAPI;
-    const requestData = {
-      ...timeDelta,
-      token: localStorage.getItem("token"),
-    };
+    const token = localStorage.getItem("token");
 
     try {
-      return await Backend.call("weight/get", requestData);
+      let weights = [];
+      let offset = 0;
+      let response = await Backend.call("weight/get", {
+        ...timeDelta,
+        token: token,
+      });
+      while (response.count !== 0) {
+        response = await Backend.call("weight/get", {
+          ...timeDelta,
+          token: token,
+          offset: offset,
+          count: 200,
+        });
+        weights = weights.concat(response.weights);
+        offset += 200;
+      }
+      return weights;
     } catch (e) {
       if (e instanceof BackendError) return rejectWithValue(e.message);
       else return rejectWithValue("Request error");
@@ -69,7 +82,7 @@ const dashboardSlice = createSlice({
     [fetchWeights.fulfilled]: (state, { payload }) => {
       state.statistics = {
         ...state.statistics,
-        ...payload,
+        ...payload.reduce((o, curr) => ({ ...o, [curr.id]: curr }), {}),
       };
       state.error = undefined;
       state.status = "idle";
