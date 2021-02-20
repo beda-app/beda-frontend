3023f898464cf72b1d9b2560a3037eb0faa9b86e
egesha <egesha@danwin1210.me>
2021-02-20 20:27:58 +0300

    :art: Rederect form index to dashboard

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 
-const Home = () => <Redirect to="/login" />;
+const Home = () => <Redirect to="/dashboard" />;
 export default Home;
